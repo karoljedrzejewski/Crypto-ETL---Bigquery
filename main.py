e447eb10fea42519b8b8d44ee6585452adcbc40f
Karol <karol.jedrzejewski10@gmail.com>
2025-05-14 13:03:45 +0200

    up

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ dotenv.load_dotenv()
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "gcp_key.json"
 GCP_PROJECT_ID = os.getenv("GCP_PROJECT_ID")
-API_KEY = os.getenv("API_KEY_GH")
+API_KEY_GH = os.getenv("API_KEY_GH")
 
 
 if __name__ == "__main__":
     try:
-        trending_data = extract.extract_data('https://api.coingecko.com/api/v3/search/trending', API_KEY)    
+        trending_data = extract.extract_data('https://api.coingecko.com/api/v3/search/trending', API_KEY_GH)    
         df = transform.transform_data(trending_data['coins'])
         load.load_df_to_gbq(df, dataset='cryptos', table_id='exchange', project_id=GCP_PROJECT_ID)
     except Exception as e:
+        print("GCP_PROJECT_ID is set:", bool(GCP_PROJECT_ID))
+        print('API_KEY_GH is set:', bool(API_KEY_GH))
         print(f"An error occurred: {e}")
         print('Response content:', trending_data)
-        print(API_KEY)
